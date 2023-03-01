1fa6144a4aad5464775c05670d74543da96218be
Tony Stark <afemiadom@icloud.com>
2023-03-01 03:17:50 -0600

    git action fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,8 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'use-aragon-doc', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
